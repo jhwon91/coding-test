df3c666130bbcb8011949c3fc179982798fbd564
WJH <55099632+jhwon91@users.noreply.github.com>
2024-06-19 23:11:21 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,8 @@
+def solution(players, callings):
+    answer = []
+    idx = -1
+    for n in range(callings):
+        idx = players.index(callings[n])
+        print(callings[n])
+        
+    return answer
