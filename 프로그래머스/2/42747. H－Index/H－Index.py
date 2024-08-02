aaa59d4d3cabf0b44eb3a46e7bfcf2f325ea471e
WJH <55099632+jhwon91@users.noreply.github.com>
2024-08-02 20:17:28 +0900

    [level 2] Title: H-Index, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,3 @@
 def solution(citations):
     answer = 0
-    
-    citations.sort(reverse = True) #내림차순 정렬
-    
-    for idx, citation in enumerate(citations):
-        if idx + 1 <= citation:
-            answer = idx + 1
-    
     return answer
