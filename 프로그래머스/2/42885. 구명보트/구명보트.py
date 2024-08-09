630e1b50270bb6bb655dae1deea23e33a19a3823
WJH <55099632+jhwon91@users.noreply.github.com>
2024-08-10 00:02:11 +0900

    [level 2] Title: 구명보트, Time: 8.78 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    
+    left = 0
+    right = len(people) - 1
+    count = 0
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+            
+        right -= 1
+        count += 1
+    
+    answer = count
+    return answer
