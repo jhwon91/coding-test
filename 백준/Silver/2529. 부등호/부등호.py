4b68038701e408b8f01fcd58fa6a844494aab1d0
WJH <55099632+jhwon91@users.noreply.github.com>
2024-08-27 14:48:41 +0900

    [Silver I] Title: 부등호, Time: 120 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -0,0 +1,28 @@
+
+
+def compare(i,j,s):
+	if s == '>':
+		return i > j
+	elif s == '<':
+		return i < j
+
+
+def backtrack(index, num):
+	if index == k + 1:
+		result.append(num)
+		return
+
+	for n in range(10):
+		if not visited[n]:
+			if index == 0 or compare(int(num[-1]),n,symbol[index - 1]):
+				visited[n] = True
+				backtrack(index + 1, num + str(n))
+				visited[n] = False
+				
+k = int(input())
+symbol = input().split()
+visited = [False] * 10
+result = []
+backtrack(0,"")
+print(max(result))
+print(min(result))
