e6cf4380d733be04c129bd62ad2dc12e56037ebe
WJH <55099632+jhwon91@users.noreply.github.com>
2024-11-03 23:29:28 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,13 +1,17 @@
 def solution(word):
     answer = 0
-    word_arr = []
-    def create_word(c_word):
-        if len(c_word) <= 5:
-            word_arr.append(c_word)
-            for i in ['A', 'E', 'I', 'O', 'U']:
-                create_word(c_word+i)
     
-    create_word('')
+    weights = [781, 156, 31, 6, 1]  # [625+125+25+5+1, 125+25+5+1, 25+5+1, 5+1, 1]
     
-    answer = word_arr.index(word)
+    dic = {
+        'A': 0,
+        'E': 1,
+        'I': 2,
+        'O': 3,
+        'U': 4
+    }
+    
+    for w in range(len(word)):
+        answer += dic[word[w]] * weights[w] + 1
+    
     return answer
