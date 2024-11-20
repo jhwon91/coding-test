11c0640717ee637383bbea1a1c5dc37dd9d6e3a1
WJH <55099632+jhwon91@users.noreply.github.com>
2024-11-20 14:14:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.53 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,34 +1,33 @@
-from collections import deque
-
-def BFS (start, graph, visited, check_link):
-    q = deque([start])
-    visited[start] = True
-    cnt = 1
-    while q:
-        s = q.popleft()
-        for adj_v in graph[s]:
-            if visited[adj_v] == False and check_link[start][adj_v]:
-                q.append(adj_v)
-                visited[adj_v] = True
-                cnt += 1
-    return cnt
 def solution(n, wires):
-    answer = n -2 
-    
-    check_link = [[True]*(n+1) for _ in range(n+1)]
+    answer = 100000
+    tempCnt = 0
     graph = [[] for _ in range(n+1)]
-
-    for s, e in wires:
-        graph[s].append(e)
-        graph[e].append(s)
     
-    for s, e in wires:
-        visited = [False]*(n+1)
-        check_link[s][e] = False
-        cnt_s = BFS(s, graph, visited, check_link)
-        cnt_e = BFS(e, graph, visited, check_link)
-        check_link[s][e] = True
+    for i,j in wires:
+        graph[i].append(j)
+        graph[j].append(i)
+    
+    for v1, v2 in wires:
+        visited = [False] * (n + 1)
         
-        answer = min(answer, abs(cnt_s- cnt_e))
-            
+        graph[v1].remove(v2)
+        graph[v2].remove(v1)
+        
+        tempCnt=abs(dfs(v1,graph,visited) - dfs(v2,graph,visited))
+        answer = min(answer, tempCnt)
+        
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+        
     return answer
+
+
+def dfs(currentNode, graph, visited):
+    visited[currentNode] = True
+    count = 1
+    
+    for i in graph[currentNode]:
+        if not visited[i]:
+            count += dfs(i, graph,visited)
+            
+    return count
