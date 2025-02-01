e343a03b018445a3367666ff0d3161ea2e9679bd
WJH <55099632+jhwon91@users.noreply.github.com>
2025-02-01 19:46:15 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 180 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(" ".repeat(N - i) + "*".repeat(i));
+        }
+        for (int i = 1; i <= N - 1; i++) {
+            System.out.println(" ".repeat(i) + "*".repeat(N - i));
+        }
+    }
+}
+
