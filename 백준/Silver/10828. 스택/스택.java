245be0329b116f89c4f412ffb78547c44b3e118b
WJH <55099632+jhwon91@users.noreply.github.com>
2025-02-04 23:26:05 +0900

    [Silver IV] Title: 스택, Time: 288 ms, Memory: 18700 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,100 +1,54 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private Node top;
-
-    public class Node{
-        int data;
-        Node next;
-
-        public Node(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    public void push(int data){
-        Node newNode = new Node(data);
-        newNode.next = top;
-        top = newNode;
-    }
-
-    public void pop(){
-        if(top == null){
-            System.out.println(-1);
-        } else {
-            System.out.println(top.data);
-            top = top.next;
-        }
-    }
-
-    public void size(){
-        if(top == null){
-            System.out.println(0);
-            return;
-        }
-
-        int count = 1;
-        Node current = top;
-        while(current.next != null){
-            count++;
-            current = current.next;
-        }
-
-        System.out.println(count);
-    }
-
-    public void empty(){
-        if(top == null){
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-    }
-
-    public void top(){
-        if(top == null){
-            System.out.println(-1);
-        } else {
-            System.out.println(top.data);
-        }
-    }
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        Main stack = new Main();
+        Stack<Integer> stack = new Stack<>();
 
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine()," ");
+            st = new StringTokenizer(br.readLine(), " ");
             String type = st.nextToken();
 
             switch (type){
                 case "push":
-                    int data = Integer.parseInt(st.nextToken());
-                    stack.push(data);
+                    stack.push(Integer.parseInt(st.nextToken()));
                     break;
+                    
                 case "pop":
-                    stack.pop();
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }else {
+                        System.out.println(stack.pop());
+                    }
                     break;
+                    
                 case "size":
-                    stack.size();
+                    System.out.println(stack.size());
                     break;
+                    
                 case "empty":
-                    stack.empty();
+                    if(stack.isEmpty()){
+                        System.out.println(1);
+                    }else {
+                        System.out.println(0);
+                    }
                     break;
                 case "top":
-                    stack.top();
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }else {
+                        System.out.println(stack.peek());
+                    }
                     break;
             }
         }
+
     }
 }
